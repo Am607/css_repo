39226c4bbbc4019b4d89c3ca1ad80c0aec1bf2fd
arshad o <ollakkanarshad@gmail.com>
2022-04-10 07:32:28 +0530

    js_file_added

---
+++ index.js
@@ -0,0 +1,15 @@
+let nam = 'akbar m mp';
+let b = nam.slice(0 , 3) //starting index and enditng index
+let c = nam.includes('akbar')
+let d  = nam.search('m')
+let f  = nam.indexOf('f')
+let g = nam.lastIndexOf('m')
+
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(f)
+// console.log(g)
+
+// array
+
